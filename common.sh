819406dcbc8f4edb481048a8f043befc5dd1b1fb
Rateria <shubham.agarwal982@gmail.com>
2022-07-21 12:20:26 +0530

    removed pending -is

--- common.sh
+++ common.sh
@@ -227,8 +227,8 @@ flash_images() {
 
   export FLASH_VID_PID=1f3a1010
   if wait_for_fastboot; then
-    ${FASTBOOT} -i -u flash UBI $IMAGESDIR/chip-$nand_erasesize-$nand_writesize-$nand_oobsize.ubi.sparse || RC=1
-    ${FASTBOOT} -i continue > /dev/null
+    ${FASTBOOT} flash UBI $IMAGESDIR/chip-$nand_erasesize-$nand_writesize-$nand_oobsize.ubi.sparse || RC=1
+    ${FASTBOOT} continue > /dev/null
   else
     echo "failed to flash the UBI image"
     RC=1
